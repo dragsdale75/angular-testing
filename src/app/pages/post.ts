98e48ffc0d986058ee0d609adba903728a8822ee
Darrin Ragsdale <darrin.ragsdale@gmail.com>
2018-04-03 07:50:27 -0500

    remove default urls

--- src/app/pages/post.ts
+++ src/app/pages/post.ts
@@ -8,8 +8,10 @@ export class Post {
 
   constructor() {
     this.name = '';
-    this.avatarUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Downtown_View_from_Sheraton_Hotel.jpg/330px-Downtown_View_from_Sheraton_Hotel.jpg';
-    this.largeImageUrl = 'http://via.placeholder.com/600x300';
+    this.avatarUrl = '';
+    this.largeImageUrl = '';
     this.message = '';
   }
+
+
 }
