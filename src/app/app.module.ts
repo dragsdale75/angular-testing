d1c0e1ae4b436a4f480ceb565491cfd33f3be0c5
Darrin Ragsdale <darrin.ragsdale@gmail.com>
2018-04-03 15:30:30 -0500

    Improved Create Post Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,6 +26,7 @@ import { AdComponent } from './pages/ad/ad.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Post } from '../post';
 import { PostService } from '../post.service';
 
@@ -9,53 +11,75 @@ import { PostService } from '../post.service';
 })
 export class CreatePostComponent implements OnInit {
 
-  newPost: Post;
-  noValidationError: Boolean;
-  errorMessage: string;
+  postForm: FormGroup;
 
-  constructor(private postService: PostService) {
+  newPost: Post;
+
+  // noValidationError: Boolean;
+  // errorMessage: string;
+
+  // avatarUrlError: string;
+  // largImageUrlError: string;
+  // messageError: string;
+
+  constructor(private postService: PostService, private fb: FormBuilder) {
     this.newPost = new Post();
-    this.noValidationError = true;
-    this.errorMessage = '';
+    // this.noValidationError = true;
+    // this.errorMessage = '';
   }
 
   ngOnInit() {
+    this.createForm();
   }
 
-  validateForm(): string {
-    this.errorMessage = '';
-    if (this.newPost.name === "") {
-      this.errorMessage += 'Please enter your name \n';
-      this.noValidationError = false;
-    }
-    if (this.newPost.avatarUrl === '') {
-      this.errorMessage += 'Please enter an Avatar URL \n';
-      this.noValidationError = false;
-    }
-    if (this.newPost.largeImageUrl === '') {
-      this.errorMessage += 'Please enter an Image URL \n';
-      this.noValidationError = false;
-    }
-    if (this.newPost.message === '') {
-      this.errorMessage += 'Please enter a Message \n';
-      this.noValidationError = false;
-    }
-
-
-    return this.errorMessage;
+  createForm() {
+    this.postForm = this.fb.group({
+      name: ['', Validators.required],
+      avatarUrl: ['', Validators.required],
+      largeImageUrl: ['', Validators.required],
+      message: ['', Validators.required]
+    });
   }
+
+
+
+// nameValidationError() {
+//   // if (this.newPost.name.length < this.newPost.minNameLength()) {
+//   //   return 'Please enter your name';
+//   // }
+
+// }
+
+
+  // validateForm(): string {
+  //   this.errorMessage = '';
+  //   if (this.newPost.name === '') {
+  //     this.errorMessage += 'Please enter your name \n';
+  //     this.noValidationError = false;
+  //   }
+  //   if (this.newPost.avatarUrl === '') {
+  //     this.errorMessage += 'Please enter an Avatar URL \n';
+  //     this.noValidationError = false;
+  //   }
+  //   if (this.newPost.largeImageUrl === '') {
+  //     this.errorMessage += 'Please enter an Image URL \n';
+  //     this.noValidationError = false;
+  //   }
+  //   if (this.newPost.message === '') {
+  //     this.errorMessage += 'Please enter a Message \n';
+  //     this.noValidationError = false;
+  //   }
+
+
+  //   return this.errorMessage;
+  // }
 
 
   onSubmit() {
-    let validationMessage = this.validateForm();
-    if (validationMessage == '') {
-      this.noValidationError = true;
       this.newPost.postDate = new Date();
       this.postService.createPost(this.newPost);
+      // Reset the form for the next entry
       this.newPost = new Post();
-    }
-
-
 
   }
 
